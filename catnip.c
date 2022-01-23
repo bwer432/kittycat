5e5cf1c7d95068b6934e87a19b95040d7f55e827
Brad Werner <brad@wernerconsulting.com>
2022-01-23 09:23:18 -0700

    catnip half-baked, just barely compiles, now the work begins

--- catnip.c
+++ catnip.c
@@ -78,6 +78,7 @@ static const char rcsid[] =
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>	// catnip for open, read, write, close, etc.
+#include <unistd.h>	// catnip for open, read, write, close, etc.
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -89,6 +90,8 @@ void printsignals __P((FILE *));
 int signame_to_signum __P((char *));
 void usage __P((void));
 static pid_t read_kitty_marker();
+static void parse_request(int);
+
 int http_trace( char* message );
 int http_head( char* message );
 int http_get( char* message );
@@ -115,6 +118,25 @@ struct method_action http_methods[] = {
 	{ "DELETE", 	http_delete },
 	{ "CONNECT", 	http_connect },
 	{ NULL,		0 }
+};
+
+enum http_version { // singular
+	HTTP_1_0,
+	HTTP_1_1,
+	HTTP_2_0,
+	HTTP_VERSION_UNKNOWN
+};
+
+struct version_map {
+	char* version;
+	enum http_version http_version;
+};
+
+struct version_map http_versions[] = { // plural
+	{ "HTTP/1.0", HTTP_1_0 },
+	{ "HTTP/1.1", HTTP_1_1 },
+	{ "HTTP/2.0", HTTP_2_0 },
+	{ NULL, HTTP_VERSION_UNKNOWN }
 };
 
 int
@@ -292,9 +314,8 @@ parse_request(int rfd)
 {
 	int off;
 	ssize_t nr, np, nw;
-	static size_t bsize = 1024; // assumed header line maximum
+	static size_t bsize = 4096; // assumed header line maximum
 	static char *buf = NULL;
-	struct stat sbuf;
 	char* p;
 	char c;
 	enum parse_state {
@@ -305,14 +326,36 @@ parse_request(int rfd)
 		WANT_HEADER_VALUE,
 		WANT_BODY,
 		ERROR_STATE
-	} state = WANT_METHOD;
+	} state;
+
+	// pointers to request components should be in a struct
+	char*	method;
+	char*	target;
+	char*	version;
+	char*	hk;
+	char*	hv;
+	char*	server;
+	char*	port;
+	struct method_action* map; // method-action-pointer = map
+	struct version_map* vp; 
+	int	e; // error code
 
 	if ((buf = malloc(bsize)) == NULL)
 		err(1, "buffer");
 	for( p = buf; (nr = read(rfd, buf, bsize)) > 0; ){
 		// NOT sscanf( p, "%s %s %s\n", &method, &target, &version );
+		state = WANT_METHOD; // that's how the request begins
 		method = p; // assumption for entering WANT_METHOD
-		for( np = 0; np < nr && ( c = *p ) != '\0'; ++p, ++np ){
+		target = NULL;
+		version = NULL;
+		hk = NULL;
+		hv = NULL;
+		server = NULL;
+		port = NULL;
+		map = NULL; // method-action pointer
+		vp = NULL; // version-map pointer
+		e = 0; // presumed innocent
+		for( np = 0; np < nr && ( c = *p ) != '\0' && e; ++p, ++np ){
 			switch( c ){
 			case ' ':
 				switch( state ){
@@ -320,6 +363,18 @@ parse_request(int rfd)
 					*p = '\0'; // terminate the method name
 					state = WANT_TARGET;
 					target = p+1; // assumption when entering WANT_TARGET
+					// check method at this point
+					for( map = http_methods; map && map->method != NULL; ++map ){
+						if( strcmp( method, map->method ) == 0 ){
+							// we have a winner, retain map value
+							break;
+						}
+					}
+					if( map->method == NULL ){
+						// no match
+						map = NULL;
+						e = 400; // bad request: method
+					}
 					break;
 				case WANT_TARGET:
 					*p = '\0'; // terminate the target (URL or short path)
@@ -329,37 +384,83 @@ parse_request(int rfd)
 				case WANT_VERSION:
 					// not expecting spaces within the HTTP version
 					// should flag an error
+					e = 505; // unsupported version, or 400 bad request
 					break;
 				case WANT_HEADER_KEY:
-					*p = '\0'; 
+					*p = '\0'; // this is *after* the colon hopefully
 					break;
 				case WANT_HEADER_VALUE:
+					// let them accumulate within the value
+					if( p == hv ){ // except
+						*p = '\0'; // eat the leading space
+						hv = p+1; // as it is ignored
+					}
+					break;
+				case WANT_BODY:
+					// whatever - we should have stopped by now
+					break;
+				case ERROR_STATE:	
+					// should be unreachable
 					break;
 				}
 				break;
 			case '\n':
 				switch( state ){
 				case WANT_VERSION:
-				case WANT_HEADER_VALUE:
-					*p = '\0'; // terminate the version or header value
+					*p = '\0'; // terminate the version 
 					state = WANT_HEADER_KEY;
+					hk = p+1; // assuming a header-key comes next
+					// check version at this point
+					for( vp = http_versions; vp && vp->version != NULL; ++vp ){
+						if( strcmp( version, vp->version ) == 0 ){
+							// we have a winner, retain vp value
+							break;
+						}
+					}
+					if( vp->version == NULL ){
+						// no match
+						vp = NULL;
+						e = 505; // unsupported version
+					}
+					break;
+				case WANT_HEADER_VALUE:
+					*p = '\0'; // terminate the header value
+					state = WANT_HEADER_KEY;
+					// preprocess this header now - must save (hk,hv)
+					fprintf( stderr, "catnip: should process header: (%s,%s)\n", hk, hv );
+					state = WANT_HEADER_KEY; // get set for the next one
+					hk = p+1; // assuming a header-key comes next
+					hv = NULL;
+					break;
+				case WANT_HEADER_KEY:
+					// if at very beginning, is beginning of body
+					if( p == hk )
+						state = WANT_BODY;
+					else
+						e = 400; // bad request - null header value
 					break;
 				default:
 					// signal unexpected newline
+					e = 400; // bad request - malformed
 					break;
 				}
 				break;
-			case ':':
-				break;
-			default:
+			default: // most other characters (non-delimiters) will ... 
+				// ...accumulate in the current field
 				break;
 			}
 		}
+		if( e ){
+			// some error in parsing
+			fprintf( stderr, "got error code %d while parsing\n", e );
+			break;
+		}
 	}
-	if (nr < 0) {
-		warn("%s", filename);
-		rval = 1;
-	}
+	fprintf( stderr, "catnip: request parsing summary; " );
+	if( method != NULL )fprintf( stderr, "method=%s; ", method );
+	if( target != NULL )fprintf( stderr, "target=%s; ", target );
+	if( version != NULL )fprintf( stderr, "version=%s; ", version );
+	fprintf( stderr, "\n" );
 }
 
 static void
